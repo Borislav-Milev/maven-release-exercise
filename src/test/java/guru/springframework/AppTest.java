73ef0e563c4eeb9cc25661fe71d2f23a6b378a8e
B.Milev <lallushe13@gmail.com>
2024-02-11 12:34:29 +0200

    test

--- src/test/java/guru/springframework/AppTest.java
+++ src/test/java/guru/springframework/AppTest.java
@@ -1,7 +1,10 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.Test;
+
 public class AppTest{
 
+    @Test
     void getHello() {
         System.out.println(new JavaHelloWorld().getHello());
     }
